643302bd324f4bec50115814e10913afd171da5f
danielle076 <d.vandenakker@novi-education.nl>
2021-03-11 09:34:59 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Begin hier met het bouwen van de applicatie!
+    </div>
+  );
+}
+
+export default App;
